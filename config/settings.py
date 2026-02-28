c238d21d58d34b40ca91025ae9e77912cd2158f8
Ivan Vostrecov <ivan.betep@gmail.com>
2026-02-28 08:55:59 +0300

    Добавлена модель пользователя в Админку, внесены необходимые изменения в настройки.

--- config/settings.py
+++ config/settings.py
@@ -126,9 +126,9 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/5.2/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'ru-ru'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Asia/Omsk'
 
 USE_I18N = True
 
@@ -144,3 +144,4 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+AUTH_USER_MODEL = 'users.User'
