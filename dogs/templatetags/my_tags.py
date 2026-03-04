890405d04333786bc5f112df91ae5f7c3388fb7a
Ivan Vostrecov <ivan.betep@gmail.com>
2026-03-04 23:34:21 +0300

    1) Исправлено строчка 9 добавлен знак /

--- dogs/templatetags/my_tags.py
+++ dogs/templatetags/my_tags.py
@@ -6,5 +6,5 @@ register = template.Library()
 @register.filter()
 def dogs_media(val):
     if val:
-        return fr'media/{val}'
+        return fr'/media/{val}'
     return '/static/dummydog.jpg'
