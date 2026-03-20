d2a867662f851cfd77bd6c72563bcde76c2f67d1
Ivan Vostrecov <ivan.betep@gmail.com>
2026-03-20 07:55:16 +0300

    1) Прописан CRUD для reviews; 2) Добавлены пустые шаблоны;

--- reviews/views.py
+++ reviews/views.py
@@ -1,5 +1,6 @@
 from django.http import HttpResponseForbidden
 from django.shortcuts import reverse, get_object_or_404, redirect
+from django.template.defaultfilters import title
 from django.views.generic import ListView, CreateView, UpdateView, DetailView, DeleteView
 from django.contrib.auth.mixins import LoginRequiredMixin, PermissionRequiredMixin
 from django.core.exceptions import PermissionDenied
@@ -7,7 +8,7 @@ from redis.commands.search.querystring import querystring
 
 from reviews.models import Review
 from reviews.forms import ReviewForm
-from users.models import User
+from users.models import UserRoles
 
 
 class ReviewListView(ListView):
@@ -34,3 +35,52 @@ class ReviewDeactivatedListView(ListView):
         queryset = super().get_queryset()
         queryset = queryset.filter(sign_of_review=False)
         return queryset
+
+
+class ReviewCreateView(LoginRequiredMixin, CreateView):
+    model = Review
+    form_class = ReviewForm
+    template_name = 'reviews/create.html'
+    extra_context = {
+        'title': 'Добавить отзыв'
+    }
+
+class ReviewDetailView(DetailView):
+    model = Review
+    template_name = 'reviews/detail.html'
+    extra_context = {
+        'title': 'Просмотр отзыва'
+    }
+
+class ReviewUpdateView(LoginRequiredMixin, UpdateView):
+    model = Review
+    form_class = ReviewForm
+    template_name = 'reviews/update.html'
+
+
+    def get_success_url(self):
+        return reverse('reviews:review_detail')
+
+    def get_object(self, queryset=None):
+        review_object = super().get_object(queryset)
+        if review_object.author != self.request.user and self.request.user not in [UserRoles.ADMIN, UserRoles.MODERATOR]:
+            raise PermissionDenied()
+        return self.object
+
+    def get_context_data(self, **kwargs):
+        context_data = super().get_context_data()
+        review_object = self.get_object()
+        context_data['title'] = f'Изменить отзыв {review_object.dog}'
+        return context_data
+
+
+class ReviewDeleteView(PermissionRequiredMixin, DeleteView):
+    model = Review
+    template_name = 'reviews/delete.html'
+    permission_required = 'reviews.delete_review'
+    extra_context = {
+        'title': 'Удалить отзыв'
+    }
+
+    def get_success_url(self):
+        return reverse('reviews:reviews_list')
